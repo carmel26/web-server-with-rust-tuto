56b45d6a1ac72fc0a6a3abf3f1ad2ce118ec1f57
carmel26 <nkeshipelagie@gmail.com>
2026-09-12 17:05:30 +0300

    defining get method to retrieve a static vehicule data

---
+++ api-test.ps1
@@ -0,0 +1,5 @@
+# get basics vehicules samples
+Invove-RestMethod -Uri "http://localhost:6579/" Method Get
+
+# post new vehicules
+Invoke-RestMethod : -Uri "http://localhost:6579/vehicule" Method Post
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use axum:: {
-    routing::get,
-    Router,
+use axum::{
+    Json, Router, debug_handler, routing::get
 };
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-    let route01 = Router::new().route("/vehicule", get (get_vehicules_hundler));
+    // create the axum router
+    let route01 = Router::new().route("/vehicule", get (get_vehicules_hundler).post(vehicule_post_handler));
 
     // 2. Define the IP and port for our listener (TCP)
     let address = "0.0.0.0:6579";
@@ -20,7 +20,26 @@ async fn main() {
         .unwrap() 
 }
 
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+struct Vehicule {
+    manufacturer : String,
+    model: String,
+    year: u16,
+    id: String,
+}
 
-async fn get_vehicules_hundler() {
+#[debug_handler]
+async fn get_vehicules_hundler() -> Json<Vehicule> {
+    println!("Caller retrieved vehicule data from Axum");
+    Json::from(
+        Vehicule{
+            manufacturer: "Toyota".to_string(),
+            model: "Corolla".to_string(),
+            year: 2020,
+            id: Uuid::new_v4().to_string(),
+        }
+    )
+}
 
+async fn vehicule_post_handler() {
 }
